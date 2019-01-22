1b2dbc31727ceff2f8d57eac7f7b71a649e496a8
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-21 17:46:20 -0800

    start testing environment tile generation, player sprite

--- Assets/Scripts/TestEnvironmentGeneration.cs
+++ Assets/Scripts/TestEnvironmentGeneration.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TestEnvironmentGeneration : MonoBehaviour
 {
+    
+    public GameObject environmentTilePrefab;
+
+    public static IDictionary<int[], GameObject> environmentTiles;
+    public int tilesWidth;
+    public int tilesHeight;
+
     // Start is called before the first frame update
     void Start()
     {
+        int halfWidth = tilesWidth / 2;
+        int halfHeight = tilesHeight / 2;
+        // proceedurally generate the game tiles
+        for (int y = -halfHeight; y < halfHeight; y++)
+        {
+            for (int x = -halfWidth; x < halfWidth; x++)
+            {
+                Vector3 position = new Vector3(x, y, 0);
+                Instantiate(environmentTilePrefab, position, transform.rotation);
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
