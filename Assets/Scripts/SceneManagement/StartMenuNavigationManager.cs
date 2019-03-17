a85efc8a2cddc99854ecb5a03d4d0b1ce68e392b
Harry Sadler <harrysadlermusic@gmail.com>
2019-03-16 23:04:17 -0700

    add start menu scene, add navigation, add lighting to 3d demo

---
+++ Assets/Scripts/SceneManagement/StartMenuNavigationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuNavigationManager : MonoBehaviour
+{
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void GoTo2dDemo() {
+        SceneManager.LoadScene("2dDemo");
+    }
+
+    public void GoTo3dDemo() {
+        SceneManager.LoadScene("3dDemo");
+    }
+
+}
